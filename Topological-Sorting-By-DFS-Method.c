a57ec514066183ae51892f77cd5c6ae3e5f82799
Faizan-Mushtaq <faizan.wani234@gmail.com>
2019-03-21 22:05:08 +0530

    Add full code.

--- Topological-Sorting-By-DFS-Method.c
+++ Topological-Sorting-By-DFS-Method.c
@@ -1,6 +1,26 @@
 #include<stdio.h>
 #include<stdlib.h>
 int j=0,pop[10],vis[10];
+
+int main()
+{
+    int i,j,n,a[10][10];
+    printf("Enter no of vertices: ");
+    scanf("%d",&n);
+    printf("Enter adjacency matrix\n");
+    for(i=1;i<=n;i++)
+    {
+    for(j=1;j<=n;j++)
+    {
+    scanf("%d",&a[i][j]);
+    }
+    }
+    topt(n,a);
+    printf("Topological Order:");
+    for(i=n;i>0;i++)
+    printf("%d",pop[i]);
+}
+
 int dfs(int u,int n,int a[][10])
 {
     int i,v,top=-1,s[10];
