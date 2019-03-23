73bfb95b76efebd461c13e1420e2b0ee4bf9d127
Faizan-Mushtaq <faizan.wani234@gmail.com>
2019-03-23 10:47:17 +0530

    Add code.

---
+++ Topological-Ordering-By-VertexDeletion-Method.c
@@ -0,0 +1,54 @@
+#include<stdio.h>
+#include<stdlib.h>
+#define MAX 10
+int topo(int,int [][MAX],int [],int []);
+int main()
+{
+    int i,j,m,n,a[MAX][MAX],b[MAX],indegree[MAX];
+    printf("ENTER NO OF VERTICIES\n");
+    scanf("%d",&n);
+    for(i=1;i<=n;i++)
+    {
+        indegree[i]=0;
+    }
+    printf("ENTER ADJACENCY MATRIX\n");
+    for(i=1;i<=n;i++)
+    {
+        for(j=1;j<=n;j++)
+        {
+            scanf("%d",&a[i][j]);
+            if(a[i][j]==1)
+            indegree[j]++;
+        }
+    }
+    m=topo(n,a,indegree,b);
+    if(m!=n)
+    {
+        printf("CYCLE DETECTED TOPOLOGICAL ORDERING NOT POSSIBLE\n");
+    }
+    else
+    {
+        printf("TOPOLOGICAL ORDERING: ");
+        for(i=1;i<=n;i++)
+        {
+            printf("%d ",b[i]);
+        }
+    }
+    return 0;
+}
+int topo(int n,int a[][MAX],int indegree[MAX],int b[MAX])
+{
+    int i,j,k=0;
+    for(i=1;i<=n;i++)
+    {
+        if(indegree[i]==1)
+        b[k++]=i;
+        for(j=1;j<=n;j++)
+        {
+            if(indegree[j]!=0 && a[i][j]==1)
+            indegree[j]--;
+        }
+        i=0;
+    }
+    return k;
+}
