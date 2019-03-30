e64bcf55d83bfed59989b3117a8e910b0e61459b
Faizan-Mushtaq <faizan.wani234@gmail.com>
2019-03-30 19:20:16 +0530

    Improve code.

--- BinomialCoefficient.c
+++ BinomialCoefficient.c
@@ -9,7 +9,7 @@ int n,k,answer;
 printf("Enter N & K value: ");
 scanf("%d%d",&n,&k);
 answer = BinomialCoefficient(n,k);
-printf("\n\nThe value of C(%d,%d) is %d.\n",n,k,answer);
+printf("\nThe value of C(%d,%d) is %d.\n",n,k,answer);
 return 0;
 }
 
@@ -19,22 +19,24 @@ return (a > b)? b:a;
 }
 int BinomialCoefficient(int n,int k)
 {
-int i,j,C[n][n],l;
+int i,j,C[n+1][n+1];
 for(i = 0;i <= n;i++)
 {
-for(j = 0,l = min(i,k);j <= l;j++)
+for(j = 0;j<= min(i,k);j++)
 {
     if(j == 0 || j == i)
     C[i][j] = 1;
     else
     C[i][j] = C[i-1][j-1] + C[i-1][j];
+    printf("%3d ",C[i][j]);
 }
+printf("\n");
 }
-for(i = 0;i <= n;i++)
+/*for(i = 0;i <= n;i++)
 {
     for(j = 0;j <= i;j++)
     printf("%d ",C[i][j]);
     printf("\n");
-}
+}*/
     return C[n][k];
 }
