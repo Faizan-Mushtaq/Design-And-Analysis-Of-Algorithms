499d49d22e14718c0fec79d0ca6e42c08dcb8bf9
Faizan-Mushtaq <faizan.wani234@gmail.com>
2019-04-29 20:03:32 +0530

    Add code.

--- Prims-MST.c
+++ Prims-MST.c
@@ -1,55 +1,55 @@
-#include <stdio.h>
-#include <stdlib.h>
-#define Inf 99
-void Prims(int,int [10][10],int);
+#include<stdio.h>
+#include<stdlib.h>
+#define inf 999
+void prims(int n,int cost[10][10],int s);
 int main()
 {
-int N,i,j,Source;
-printf("Enter the number of nodes: ");
-scanf("%d",&N);
-int Cost[10][10];
-printf("Enter the cost of the edges:\n");
-for(i = 1;i <= N;i++)
-for(j= 1;j <= N;j++)
-scanf("%d",&Cost[i][j]);
-printf("Enter the source vertex: ");
-scanf("%d",&Source);
-Prims(N,Cost,Source);
-return 0;
+    int n,i,j,source,cost[10][10];
+    printf("enter no of nodes: ");
+    scanf("%d",&n);
+    printf("enter cost matrix\n");
+    for(i=1;i<=n;i++)
+    for(j=1;j<=n;j++)
+    scanf("%d",&cost[i][j]);
+    printf("enter source vet: ");
+    scanf("%d",&source);
+    prims(n,cost,source);
+    return 0;
 }
-void Prims(int N,int Cost[10][10],int S)
+void prims(int n,int cost[10][10],int s)
 {
-int i,j,U,sum = 0,Distance[10],Visit[10],Vertex[10],least;
-for(i = 1;i <= N;i++)
-{
-Visit[i] = 0;
-Distance[i] = Cost[S][i];
-Vertex[i] = S;
-}
-Visit[S] = 1;
-printf("Edges in MST:");
-for(i = 1;i < N;i++)
-{
-least = Inf;
-for(j = 1;j <= N;j++)
-{
-if(Visit[j] == 0 && Distance[j] < least)
-{
-least = Distance[j];
-U = j;
-}
-}
-Visit[U] = 1;
-sum = sum + Distance[U];
-printf("\nThe edge (%d,%d) cost =%d",Vertex[U],U,Distance[U]);
-for(j = 1;j <= N;j++)
-{
-if(Visit[j] == 0 && Cost[U][j] < Distance[j])
-{
-Distance[j] = Cost[U][j];
-Vertex[j] = U;
-}
-}
-}
-printf("\nTotal cost MST =%d",sum);
+    int vis[10],vet[10],dis[10];
+    int sum=0,i,j,min,v;
+    for(i=1;i<=n;i++)
+    {
+        vis[i]=0;
+        dis[i]=cost[s][i];
+        vet[i]=s;
+    }
+    vis[s]=1;
+    printf("edge in mst\n");
+    for(i=1;i<n;i++)
+    {
+        min=inf;
+        for(j=1;j<=n;j++)
+        {
+            if(vis[j]==0 && dis[j]<min)
+            {
+                min=dis[j];
+                v=j;
+            }
+        }
+        vis[v]=1;
+        sum=sum+dis[v];
+        printf("Edge (%d,%d):cost=%d\n",vet[v],v,dis[v]);
+        for(j=1;j<=n;j++)
+        {
+            if(vis[j]==0&& cost[v][j]<dis [j])
+            {
+                dis[j]=cost[v][j];
+                vet[j]=v;
+            }
+        }
+    }
+    printf("\ntotal cost of mst=%d",sum);
 }
