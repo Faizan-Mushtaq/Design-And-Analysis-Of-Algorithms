4d5da183bec8954ccc5abda900db543cbe69b786
Faizan-Mushtaq <faizan.wani234@gmail.com>
2019-05-01 15:54:33 +0530

    Improve for all instances of pattern.

--- HorsePoolStringMatching.c
+++ HorsePoolStringMatching.c
@@ -10,27 +10,34 @@
     	  t[i]=m;
     	for (j=0;j<m-1;j++)
     	  t[p[j]]=m-1-j;
-          
+          printf("Shifttable\n");
+          for(int k=0;k<m;k++)
+    printf("t[%d]=%d ",k+1,t[p[k]]);
+
     }
     int horspool(char src[],char p[]) {
-    	int i,j,k,m,n;
+    	int i,j,k,m,n,flag=0;
     	n=strlen(src);
     	m=strlen(p);
-    	printf("\nLength of text=%d",n);
-    	printf("\nLength of pattern=%d",m);
+    	printf("\nLength of text=%d\n",n);
+    	printf("Length of pattern=%d\n",m);
     	i=m-1;
     	while(i<n) {
     		k=0;
     		while((k<m)&&(p[m-1-k]==src[i-k]))
     		   {k++;}
-    		if(k==m)
-    		   return(i-m+1);
+    		if(k==m){
+                flag=1;
+                printf("pattern found at %d\n",i-m+2);
+                i+=t[src[i]];
+    		  // return(i-m+1);
+          }
                 else
     		   i+=t[src[i]];
     	}
-    	return -1;
+    	return flag;
     }
-    void main() {
+    int main() {
     	char src[100],p[100];
     	int pos;
     	printf("Enter the text in which pattern is to be searched:\n");
@@ -40,8 +47,9 @@
     	scanf("%[^\n]",p);//gets(p);
     	shifttable(p);
     	pos=horspool(src,p);
-    	if(pos>=0)
-    	  printf("\nThe desired pattern was found starting from position %d",pos+1);
-        else
+    	//if(pos>=0)
+    	  //printf("\nThe desired pattern was found starting from position %d",pos+1);
+        if(pos==0)
     	  printf("\nThe pattern was not found in the given text\n");
+           return 0;
     }
